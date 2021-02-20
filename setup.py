ca0cf1f8b1b989b2030060f58ce63d4dfe6c46b7
yoddoytoo <77707403+yoddoytoo@users.noreply.github.com>
2021-02-21 00:15:02 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except:
 setup(
   name = 'yoddoy01test',         # How you named your package folder (MyLib)
   packages = ['yoddoy01test'],   # Chose the same as "name"
-  version = 'Beta 0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'OOP Learning Example by Yoddoy',   # Give a short description about your library
   long_description=long_description,
